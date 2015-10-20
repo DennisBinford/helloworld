c60440934f883cc1fac3e63b96357f0839eb868e
S10048881 <dennis@binford.org>
2015-10-20 12:36:52 -0500

    bundled the two programs

--- app/src/main/java/com/example/jchuah/myapplication/ColorActivity.java
+++ app/src/main/java/com/example/jchuah/myapplication/ColorActivity.java
@@ -1,40 +1,22 @@
 package com.example.jchuah.myapplication;
 
-import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
-        import android.os.Bundle;
-        import android.util.Log;
-        import android.view.Menu;
-        import android.view.MenuItem;
-        import android.view.View;
-        import android.widget.Toast;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
-public class MainActivity extends AppCompatActivity {
-
-    static String tag = "com.example.jchuah.myapplication.MainActivity";
-
-
-
-    Bundle sendBundle = new Bundle();
+public class ColorActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-    }
-
-    public void onNextClick(View source) {
-        //Toast.makeText(this, "Howdy", Toast.LENGTH_LONG).show();
-
-        Log.i(tag, "Launching Color Activity");
-        Intent colorActivityIntent = new Intent(this, ColorActivity.class);
-        startActivity(colorActivityIntent,sendBundle);
+        setContentView(R.layout.activity_color);
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
+        getMenuInflater().inflate(R.menu.menu_color, menu);
         return true;
     }
 
