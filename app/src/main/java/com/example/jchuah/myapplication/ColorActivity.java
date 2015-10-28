1713480bedec4417a2f55bcd29b27159ebf745b5
S10048881 <dennis@binford.org>
2015-10-28 12:24:06 -0500

    addedname and color activity

--- app/src/main/java/com/example/jchuah/myapplication/ColorActivity.java
+++ app/src/main/java/com/example/jchuah/myapplication/ColorActivity.java
@@ -5,36 +5,46 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
 public class ColorActivity extends AppCompatActivity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_color);
-        Bundle groceries = getIntent().getBundleExtra("groceries");
-        Log.i("GROCERIES!", groceries.getString("name"));
-    }
+    public class NameColorActivity extends AppCompatActivity {
+        Bundle groceries;
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_color, menu);
-        return true;
-    }
+        public void onColorClick(View button) {
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
+        }
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_color);
+            Bundle groceries = getIntent().getBundleExtra("groceries");
+            Log.i("GROCERIES!", groceries.getString("name"));
+        }
+
+        @Override
+        public boolean onCreateOptionsMenu(Menu menu) {
+            // Inflate the menu; this adds items to the action bar if it is present.
+            getMenuInflater().inflate(R.menu.menu_color, menu);
             return true;
         }
 
-        return super.onOptionsItemSelected(item);
+        @Override
+        public boolean onOptionsItemSelected(MenuItem item) {
+            // Handle action bar item clicks here. The action bar will
+            // automatically handle clicks on the Home/Up button, so long
+            // as you specify a parent activity in AndroidManifest.xml.
+            int id = item.getItemId();
+
+            //noinspection SimplifiableIfStatement
+            if (id == R.id.action_settings) {
+                return true;
+            }
+
+            return super.onOptionsItemSelected(item);
+        }
+
     }
 }
